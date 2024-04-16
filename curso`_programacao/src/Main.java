6cfcebda2d7aaaaa5952f75856b21491f2613001
Romulo Nogueira de Souza <romulonogueira929@gmail.com>
2024-04-16 17:29:55 -0300

    Add files via upload

---
+++ curso`_programacao/src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Ola mundo!");
+		/*Operadores Aritmeticos*/
+		System.out.println((3+2*4));
+		System.out.println((3+2)*4);
+		System.out.println (60/(3+2)*4);
+		System.out.println (60/((3+2)*4));
+		System.out.println (14%3);
+		System.out.println (19%4); 
+		//Operadores soma,subtração,multiplicação,divisão e resto de divisão.
+		//Precedência entre os operadores.
+		//Uso de parênteses.
+		//Exemplos de expressões.
+	
+	}
+}
+	
+
+
+
